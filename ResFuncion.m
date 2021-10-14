4e2c0691b39649c229954f0e8cfc54cab3b2c374
sarailucas <92003848+sarailucas@users.noreply.github.com>
2021-10-14 15:23:56 -0500

    Agregar comentario
    Se agregaron dos comentarios para especificar la linea de codigo

--- ResFuncion.m
+++ ResFuncion.m
@@ -1,18 +1,18 @@
 %Octave Script
-%Title	    	:Resolucion de Desigualdades
+%Title		:Resolucion de Desigualdades
 %Description	:Script que resuelve una funcion
-%Authors    	:Ximena Ixel Garcia Agustin
-%	          	:Sarai Lucas Nieto
-%	          	:Suseth Abigail Sandoval Damian
-%Date		      :20211013
-% Version	    :1
-% Usage		    :octave> /path/ResFuncion.m
-% Notes		     :Se requiere aplicacion Octave
+%Authors	:Ximena Ixel Garcia Agustin
+%		:Sarai Lucas Nieto
+%		:Suseth Abigail Sandoval Damian 
+%Date		:20211013
+% Version	:1
+% Usage		:octave> /path/ResFuncion.m
+% Notes		:Se requiere plicacion Octave
 
-% Limpiar variables
+% Limpiar las variables
 clear
 
-%Calcular el minimo de cable de la punta de poste al suelo y a la punta de otro poste
+%Calcular minimo de cable de la punta de poste al suelo y a punta de otro poste
 
 %Rango de 0 a 50 en i = 1
 b=0:1:20;
@@ -20,7 +20,7 @@ b=0:1:20;
 % Obtener valor de la función
 c=@(b) (sqrt(225+(b.^2)))+(sqrt(500-(40*b)+(b.^2)));
 
-% Funcion a graficar
+% Funcion para graficar
 y= (sqrt(225+(b.^2))+(sqrt(500-(40*b)+(b.^2))));
 
 % Funcion que determina el valor minimo
@@ -30,16 +30,17 @@ valmin = fminbnd(c,0,20);
 plot(b, y);
 grid
 
-% Titulo de la grafica
+% Titulo de grafica final
 title(['Cable minimo en b = ' num2str(valmin)]);
 
-% Etiqueta para eje x
+% Etiqueta para eje de x
 xlabel(['Min b = ' num2str(valmin)]);
 
-% Etiqueta para eje y
+% Etiqueta para eje de y
 ylabel(['Min y = ' num2str(c(valmin))]);
 
-% Salida del programa
+% Salidas
+% Resultado final
 p = 'Dos postes verticales de 15m y 10m son separados por 20m';
 cal = 'Se calcula la longitud minima de un cable desde la punta de un poste al suelo y a la punta del otro poste';
 disp('');
@@ -48,5 +49,5 @@ disp('');
 disp(['Lo que se calcula es: ', num2str(cal)]);
 disp('');
 disp(['El valor minimo del cable es = ', num2str(c(valmin))]);
-
+%Final del programa
 
